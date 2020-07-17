943c7bafa898dd63212051242870d7c6b93e27b5
baba_s <shotababa1206@gmail.com>
2020-07-17 16:31:30 +0900

    first commit

---
+++ Scripts/UIDebugButton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Kogane.Internal
+{
+	/// <summary>
+	/// デバッグボタンの UI を管理するクラス
+	/// </summary>
+	[AddComponentMenu( "" )]
+	[DisallowMultipleComponent]
+	internal sealed class UIDebugButton : MonoBehaviour
+	{
+		//====================================================================================
+		// 変数(SerializeField)
+		//====================================================================================
+		[SerializeField] private Button m_buttonUI = default;
+		[SerializeField] private Text   m_textUI   = default;
+
+		//====================================================================================
+		// 関数
+		//====================================================================================
+		/// <summary>
+		/// 表示を設定します
+		/// </summary>
+		public void SetDisp( UDPData data )
+		{
+			SetDisp( data.Text, data.OnClick );
+		}
+
+		/// <summary>
+		/// 表示を設定します
+		/// </summary>
+		public void SetDisp( string text, Action onClick )
+		{
+			m_buttonUI.onClick.RemoveAllListeners();
+			m_buttonUI.onClick.AddListener( () => onClick() );
+
+			m_textUI.text = text;
+		}
+	}
+}
---
+++ Scripts/UIDebugPanel.cs
@@ -0,0 +1,111 @@
+﻿using Kogane.Internal;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+#pragma warning disable 0649
+#pragma warning disable 0414
+
+namespace Kogane
+{
+	/// <summary>
+	/// デバッグパネルの UI を管理するクラス
+	/// </summary>
+	[AddComponentMenu( "" )]
+	[DisallowMultipleComponent]
+	public sealed class UIDebugPanel : MonoBehaviour
+	{
+		//====================================================================================
+		// 定数
+		//====================================================================================
+		private const string ENABLE_SYMBOL_NAME = "ENABLE_DEBUG_PANEL";
+
+		//====================================================================================
+		// 変数(SerializeField)
+		//====================================================================================
+		[SerializeField] private GameObject    m_closeBaseUI   = default;
+		[SerializeField] private GameObject    m_openBaseUI    = default;
+		[SerializeField] private Button        m_closeButtonUI = default;
+		[SerializeField] private Button        m_openButtonUI  = default;
+		[SerializeField] private LayoutGroup   m_layoutUI      = default;
+		[SerializeField] private UIDebugButton m_buttonUI      = default;
+		[SerializeField] private CanvasGroup   m_canvasGroup   = default;
+		[SerializeField] private GameObject    m_root          = default;
+
+		//====================================================================================
+		// 変数
+		//====================================================================================
+
+		//====================================================================================
+		// 関数
+		//====================================================================================
+		/// <summary>
+		/// 初期化される時に呼び出されます
+		/// </summary>
+		private void Awake()
+		{
+#if ENABLE_DEBUG_PANEL
+			m_closeButtonUI.onClick.AddListener( () => SetState( false ) );
+			m_openButtonUI .onClick.AddListener( () => SetState( true  ) );
+#else
+			Destroy( gameObject );
+#endif
+		}
+
+		/// <summary>
+		/// 開始する時に呼び出されます
+		/// </summary>
+		private void Start()
+		{
+			m_root.SetActive( true );
+			m_openBaseUI.SetActive( false );
+			m_closeBaseUI.SetActive( true );
+
+			SetState( false );
+		}
+
+		/// <summary>
+		/// ステートを設定します
+		/// </summary>
+		[Conditional( ENABLE_SYMBOL_NAME )]
+		private void SetState( bool isOpen )
+		{
+			m_openBaseUI.SetActive( isOpen );
+			m_closeBaseUI.SetActive( !isOpen );
+		}
+
+		/// <summary>
+		/// 表示するかどうかを設定します
+		/// </summary>
+		[Conditional( ENABLE_SYMBOL_NAME )]
+		public void SetVisible( bool isVisible )
+		{
+			var alpha = isVisible ? 1 : 0;
+			m_canvasGroup.alpha = alpha;
+		}
+
+		/// <summary>
+		/// 表示を設定します
+		/// </summary>
+		[Conditional( ENABLE_SYMBOL_NAME )]
+		public void SetDisp( params UDPData[] list )
+		{
+			foreach ( Transform n in m_layoutUI.transform )
+			{
+				Destroy( n.gameObject );
+			}
+
+			m_buttonUI.gameObject.SetActive( true );
+
+			for ( int i = 0; i < list.Length; i++ )
+			{
+				var data = list[ i ];
+				var obj  = Instantiate( m_buttonUI, m_layoutUI.transform );
+
+				obj.SetDisp( data );
+			}
+
+			m_buttonUI.gameObject.SetActive( false );
+		}
+	}
+}
